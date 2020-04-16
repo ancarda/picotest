42974f6a722f83840334f156ba23654f5d40697b
Mark Dain <mark@markdain.net>
2020-04-16 22:42:03 +0100

    Wrap strings for easier comparison

--- lib/assertions.h
+++ lib/assertions.h
@@ -17,7 +17,7 @@
 // Checks that x == y. Suitable for strings (char*).
 #define ASSERT_STR_EQ(x, y) do { __TRY_ASSERT({ \
     if (strcmp(x, y) != 0) { \
-        printf("%sLine %d: ASSERT_STR_EQ: %s != %s", __ERR_MARKER, __LINE__, x, y); \
+        printf("%sLine %d: ASSERT_STR_EQ: {%s} != {%s}", __ERR_MARKER, __LINE__, x, y); \
         __ASSERTION_FAILED; \
     }}); } while (0)
 
